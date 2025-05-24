3b017aa35282ac87e1c0b4ee1d34d75dff25a16e
junesfran <junesfran@gmail.com>
2025-05-24 12:21:39 +0200

    vaciado todo papa

--- API/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from app.routes.test import test
-app.register_blueprint(test)
-
-from app.routes.prueba import prueba
-app.register_blueprint(prueba)
